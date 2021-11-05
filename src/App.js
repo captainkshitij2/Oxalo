70b7f5f6adfad6b4658c4ef12d2990318c3d7f9b
kshitij <kshitij4242@gmail.com>
2021-11-05 20:47:34 +0530

    added inputs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Dashboard from './hr';
 
 function App() {
   return (
     <div className="App">
-      hello
-      
+     <Dashboard/> 
+
     </div>
   );
 }
---
+++ src/hr.js
@@ -0,0 +1,31 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+
+function Dashboard ()  {
+
+// const [cap,setCap]= useState[''];
+
+return (
+<>
+
+
+<div>
+<input type='text' placeholder='write here' />
+
+
+
+
+</div>
+
+</>
+    
+
+)
+
+
+
+};
+
+export default Dashboard ; 
