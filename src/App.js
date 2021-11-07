cd2e31864ab65f31e535d45eca8cf84010073170
kshitij <kshitij4242@gmail.com>
2021-11-07 12:03:33 +0530

    added more components ' listing'

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter,Route,Switch } from 'react-router-dom';
 import Applicant from './Applicant';
 import Create from './Create';
 import HomePage from './HomePage';
+import List from './List';
 
 
 function App() {
@@ -14,14 +15,13 @@ function App() {
   <Switch>
 
 
-  <Route exact path="/" component={HomePage}
-        
-         ></Route>
+  <Route exact path="/" component={HomePage} ></Route>
 
 
    <Route exact path = '/Create' component={Create}></Route>
 
    <Route exact path = '/Applicant' component={Applicant}></Route>
+
 
 
   </Switch>
